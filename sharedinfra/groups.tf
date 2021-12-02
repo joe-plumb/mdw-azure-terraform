2abcf46185855540356e6000658e45650a1efe20
Joe Plumb <26112065+joe-plumb@users.noreply.github.com>
2021-12-02 17:25:11 +0000

    updating shared infra (#12)

--- sharedinfra/groups.tf
+++ sharedinfra/groups.tf
@@ -2,6 +2,12 @@ data "azuread_client_config" "current" {}
 
 resource "azuread_group" "tf-ado-syn-admins" {
   display_name     = "tf-ado-syn-admins"
-  owners           = [data.azuread_client_config.current.object_id]
+  mail_enabled     = true
+  mail_nickname    = "tfadosynadmins"
   security_enabled = true
+  types            = ["Unified"]
+
+  owners = [
+    data.azuread_client_config.current.object_id
+  ]
 }
